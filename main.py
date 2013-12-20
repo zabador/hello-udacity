fa820782651c3639928f8491cc9ac5d5529740db
zabador <zabador2000@gmail.com>
2013-12-20 13:33:12 -0500

    added a redirect

--- main.py
+++ main.py
@@ -34,9 +34,9 @@ form="""
 <form method='post'> 
 	What is you birthday
 	<br>
-	<input type="text" name="month" value="%(month)s">
-	<input type="text" name="day" value="%(day)s">
-	<input type="text" name="year" value="%(year)s">
+	<label>Month <input type="text" name="month" value="%(month)s"></label>
+	<label>Day <input type="text" name="day" value="%(day)s"></label>
+	<label>Year <input type="text" name="year" value="%(year)s"></label>
 	<div style="color: red">%(error)s</div>
 	<input type='submit'>
 </form>
@@ -44,9 +44,9 @@ form="""
 class MainPage(webapp2.RequestHandler):
 
 	def write_form(self, error="", month="", day="", year=""):
-		self.response.out.write(form % {"error": cgi.escape(error), "month": cgi.escape(month), "day": cgi.escape(day), "year": cgi.escape(year)})
+		self.response.out.write(form % {"error": error, "month": cgi.escape(month), "day": cgi.escape(day), "year": cgi.escape(year)})
 	def get(self):
-		self.response.out.write(form)
+		self.write_form(form)
 	def post(self):
 		user_month = self.request.get('month')
 		user_day = self.request.get('day')
@@ -59,9 +59,13 @@ class MainPage(webapp2.RequestHandler):
 		if not (month and day and year):
 			self.write_form("That is not valid", user_month, user_day, user_year)
 		else:
-			self.write_form("Thanx that is a valid day")
+			self.redirect("/thanks")
 
+class ThanksHandler(webapp2.RequestHandler):
+	def get(self):
+		self.response.out.write("Thanks! That's is a valid day")
 
 app = webapp2.WSGIApplication([
 	('/', MainPage),
+	('/thanks', ThanksHandler),
 ], debug=True)
